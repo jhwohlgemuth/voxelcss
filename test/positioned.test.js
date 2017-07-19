a254e2cd277b07118505092ab8c42ed47995d843
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-07-19 17:59:06 -0500

    Added more tests and refactored some code

---
+++ test/positioned.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const eventListener = require('../lib/eventListener');
+const positioned    = require('../lib/positioned');
+
+describe('Positioned module', function() {
+    let item;
+    beforeEach(() => {
+        item = positioned(eventListener({}));
+    });
+    it('can get and set position', () => {
+        expect(item.getPosition()).toEqual({x: 0, y: 0, z: 0});
+        item.setPosition(1, 2, 3);
+        expect(item.getPosition()).toEqual({x: 1, y: 2, z: 3});
+    });
+    it('can translate in 3-dimensions', () => {
+        expect(item.getPosition()).toEqual({x: 0, y: 0, z: 0});
+        item.setPosition(1, 2, 3);
+        item.translate(1, 2, 3);
+        expect(item.getPosition()).toMatchSnapshot();
+        item.translate(-2, -4, -6);
+        expect(item.getPosition()).toEqual({x: 0, y: 0, z: 0});
+    });
+    it('can only translate number values', () => {
+        expect(item.getPosition()).toEqual({x: 0, y: 0, z: 0});
+        item.translate('not', 'a', 'number');
+        expect(item.getPosition()).toEqual({x: 0, y: 0, z: 0});
+        item.translate(undefined, undefined, undefined);
+        expect(item.getPosition()).toEqual({x: 0, y: 0, z: 0});
+    });
+});
--- test/voxel.test.js
+++ test/voxel.test.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const Voxel = require('../lib/Voxel');
+const ColorFace   = require('../lib/ColorFace');
+const LightSource = require('../lib/LightSource');
+const Mesh        = require('../lib/Mesh');
+const Voxel       = require('../lib/Voxel');
 
 describe('Voxel', function() {
     let voxel;
@@ -9,5 +12,47 @@ describe('Voxel', function() {
     });
     it('can be instantiated with dimension 0', () => {
         expect(voxel.getDimension()).toEqual(0);
+    });
+    it('can create clones', function() {
+        let original = new Voxel(123, 456, 789);
+        let clone = original.clone();
+        expect(clone.getPosition()).toMatchSnapshot();
+    });
+    it('can get and set mesh', () => {
+        let oldmesh = voxel.getMesh();
+        let newMesh = new Mesh(new ColorFace('#333'))
+        expect(oldmesh).toMatchSnapshot();
+        voxel.setMesh(newMesh);
+        expect(voxel.getMesh()).toMatchSnapshot();
+    });
+    it('can update light sources', () => {
+        let mesh = new Mesh(new ColorFace('#333'))
+        voxel.updateLightSource(new LightSource());
+        voxel.setMesh(mesh);
+        expect(voxel.getMesh()).toMatchSnapshot();
+    });
+    it('can be added to and removed from a scene', function() {
+        expect(() => {
+            voxel.addToScene();
+        }).toThrowErrorMatchingSnapshot();
+        let scene = {
+            add: jest.fn(),
+            remove: jest.fn()
+        };
+        voxel.addToScene(scene);
+        expect(scene.add).toHaveBeenCalledWith(voxel);
+        voxel.removeFromScene();
+        expect(scene.remove).toHaveBeenCalledWith(voxel);
+        voxel.setParentScene();
+        voxel.removeFromScene();
+        expect(scene.remove).toHaveBeenCalledTimes(1);
+    });
+    it('can get and set dimensions', function() {
+        let dim = 42;
+        expect(voxel.getDimension()).toEqual(0);
+        voxel.setDimension(dim);
+        expect(voxel.getDimension()).toEqual(dim);
+        voxel.setDimension('not a number');
+        expect(voxel.getDimension()).toEqual(dim);
     });
 });
