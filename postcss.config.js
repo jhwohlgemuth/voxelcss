2546804295d2ab8f985b8f42f698b335b21eb3d6
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-07-21 18:23:40 -0500

    Broke up CSS into separate files for readability (added postcss-import to build chain)

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@
 module.exports = {
     parser: require('postcss-safe-parser'),
     plugins: [
+        require('postcss-import')(),
         require('postcss-cssnext')({browsers: 'last 2 versions'}),
         require('cssnano')(),
         require('postcss-reporter')({clearReportedMessages: true})
