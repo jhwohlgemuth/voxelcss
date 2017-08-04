7f8a9b3bdadc4ae0ae53fd94fa73492dd1bd1e5d
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-08-03 19:40:15 -0500

    voxelcss.js.org deployment

---
+++ test/editor.test.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const {createVoxelMock, createWorldMock, reset} = require('./mocks');
+const Voxel  = require('../lib/Voxel');
+const Scene  = require('../lib/Scene');
+const World  = require('../lib/World');
+const Editor = require('../lib/Editor');
+
+const sideClickEvents = [
+    'TopClick',
+    'BottomClick',
+    'FrontClick',
+    'BackClick',
+    'LeftClick',
+    'RightClick'
+];
+const allEvents = sideClickEvents.concat(
+    'MeshChange',
+    'VoxelClick'
+);
+
+describe('Editor', function() {
+    let editor;
+    let world;
+    let voxels = [
+        new Voxel(),
+        new Voxel(),
+        new Voxel()
+    ];
+    voxels.forEach(voxel => {
+        voxel.addEventListener = jest.fn();
+    });
+    beforeEach(() => {
+        world = createWorldMock();
+        editor = new Editor(world);
+    });
+    afterEach(() => {
+        localStorage.setItem.mockClear();
+        localStorage.getItem.mockClear();
+        reset(world);
+    });
+    it('can throw an error if instantiated without a world instance', () => {
+        expect(() => {
+            editor = new Editor();
+        }).toThrowErrorMatchingSnapshot();
+    });
+    it('can be instantiated with autosave on', () => {
+        editor = new Editor(world, {autosave: true});
+        expect(editor.canAutoSave()).toBeTruthy();
+    });
+    it('can be enabled and disabled', () => {
+        expect(editor.isEnabled()).toBeTruthy();
+        editor.disable();
+        expect(editor.isEnabled()).toBeFalsy();
+        editor.enable();
+        expect(editor.isEnabled()).toBeTruthy();
+    });
+    it('can enable and disable autosave', () => {
+        expect(editor.canAutoSave()).toBeFalsy();
+        editor.enableAutoSave();
+        expect(editor.canAutoSave()).toBeTruthy();
+        editor.disableAutoSave();
+        expect(editor.canAutoSave()).toBeFalsy();
+    });
+    it('can create, delete, load, and verify saves', () => {
+        expect(editor.isSaved()).toBeFalsy();
+        expect(world.isSaved).toHaveBeenCalledTimes(1);
+        editor.save();
+        expect(world.save).toHaveBeenCalledTimes(1);
+        editor.deleteSave();
+        expect(world.deleteSave).toHaveBeenCalledTimes(1);
+        editor.load();
+        expect(world.load).toHaveBeenCalledTimes(1);
+    });
+    it('can import and export world', () => {
+        editor.export();
+        expect(world.export).toHaveBeenCalledTimes(1);
+        let newScene = new Scene();
+        let newWorld = new World(newScene);
+        voxels.forEach(voxel => {
+            newWorld.add(voxel);
+        });
+        let serializedWorld = newWorld.export();
+        expect(serializedWorld).toMatchSnapshot();
+        editor.import(serializedWorld);
+        expect(editor.getWorld().getVoxels().length).toEqual(voxels.length);
+    });
+    it('can add voxels', () => {
+        let a = new Voxel();
+        let b = new Voxel();
+        a.addEventListener = jest.fn();
+        b.addEventListener = jest.fn();
+        editor.add(a);
+        expect(world.save).not.toHaveBeenCalled();
+        expect(a.addEventListener).toHaveBeenCalledTimes(allEvents.length);
+        editor.enableAutoSave();
+        editor.add(b);
+        expect(world.save).toHaveBeenCalled();
+        expect(b.addEventListener).toHaveBeenCalledTimes(allEvents.length);
+    });
+    it('can remove voxels', () => {
+        let a = new Voxel();
+        let b = new Voxel();
+        a.removeEventListener = jest.fn();
+        b.removeEventListener = jest.fn();
+        editor.add(a);
+        editor.add(b);
+        editor.remove(a);
+        expect(world.save).not.toHaveBeenCalled();
+        expect(a.removeEventListener).toHaveBeenCalledTimes(allEvents.length);
+        editor.enableAutoSave();
+        editor.remove(b);
+        expect(world.save).toHaveBeenCalledTimes(1);
+        expect(b.removeEventListener).toHaveBeenCalledTimes(allEvents.length);
+    });
+    it('can bind and unbind event handlers to voxels', () => {
+        let voxel = new Voxel();
+        let target = createVoxelMock();
+        editor.add(voxel);
+        allEvents.forEach(name => voxel.triggerEvent(name, {target}));
+        expect(world.save).not.toHaveBeenCalled();
+        editor.enableAutoSave();
+        editor.add(voxel);
+        allEvents.forEach(name => voxel.triggerEvent(name, {target}));
+        expect(world.save).toHaveBeenCalledTimes(allEvents.length + 1);
+        expect(target.translate).toHaveBeenCalledTimes(2 * sideClickEvents.length);
+        target.translate.mockClear();
+    });
+    it('will only handle click events when enabled', () => {
+        let voxel = new Voxel();
+        let target = createVoxelMock();
+        editor.disable();
+        editor.add(voxel);
+        allEvents.forEach(name => voxel.triggerEvent(name, {target}));
+        expect(target.translate).not.toHaveBeenCalled();
+    });
+});
---
+++ test/mocks.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Scene  = require('../lib/Scene');
+const World  = require('../lib/World');
+
+module.exports = {
+    createVoxelMock,
+    createWorldMock,
+    reset
+};
+
+function createVoxelMock() {
+    return Object.assign(document.createElement('div'), {
+        getDimension: jest.fn().mockReturnValue(100),
+        clone: jest.fn().mockReturnThis(),
+        translate: jest.fn().mockReturnThis(),
+        getDomElement: jest.fn().mockReturnThis(),
+        setParentScene: jest.fn(),
+        removeParentScene: jest.fn(),
+        addEventListener: jest.fn(),
+        updateLightSource: jest.fn()
+    });
+}
+function createWorldMock() {
+    let scene = new Scene();
+    let world = new World(scene);
+    return Object.assign(world, {
+        save: jest.fn(),
+        deleteSave: jest.fn(),
+        isSaved: jest.fn(),
+        export: jest.fn(),
+        load: jest.fn()
+    });
+}
+function reset(world) {
+    world.save.mockClear();
+    world.deleteSave.mockClear();
+    world.isSaved.mockClear();
+    world.export.mockClear();
+    world.load.mockClear();
+}
